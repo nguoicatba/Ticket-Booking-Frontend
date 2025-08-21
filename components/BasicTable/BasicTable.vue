e272ae77dd2dab1d8217778b0876582aed7104a7
Nguyễn Lê Trung Kiên <thanheco1978@gmail.com>
2025-08-21 09:42:57 +0700

    Add BasicTable component and integrate Ant Design Vue

---
+++ components/BasicTable/BasicTable.vue
@@ -0,0 +1,96 @@
+<template>
+  <a-table
+    :columns="columns"
+    :row-key="record => record.login.uuid"
+    :data-source="dataSource"
+    :pagination="pagination"
+    :loading="loading"
+    @change="handleTableChange"
+  >
+    <template #bodyCell="{ column, text }">
+      <template v-if="column.dataIndex === 'name'">{{ text.first }} {{ text.last }}</template>
+    </template>
+  </a-table>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue';
+import type { TableProps } from 'ant-design-vue';
+import { usePagination } from 'vue-request';
+import axios from 'axios';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: true,
+    width: '20%',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    filters: [
+      { text: 'Male', value: 'male' },
+      { text: 'Female', value: 'female' },
+    ],
+    width: '20%',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+];
+
+type APIParams = {
+  results: number;
+  page?: number;
+  sortField?: string;
+  sortOrder?: number;
+  [key: string]: any;
+};
+type APIResult = {
+  results: {
+    gender: 'female' | 'male';
+    name: string;
+    email: string;
+  }[];
+};
+
+const queryData = (params: APIParams) => {
+  return axios.get<APIResult>('https://randomuser.me/api?noinfo', { params });
+};
+
+const {
+  data,
+  run,
+  loading,
+  current,
+  pageSize,
+} = usePagination(queryData, {
+  pagination: {
+    currentKey: 'page',
+    pageSizeKey: 'results',
+  },
+});
+
+const dataSource = computed(() => data.value?.data?.results ?? []);
+
+const pagination = computed(() => ({
+  total: 200,
+  current: current.value,
+  pageSize: pageSize.value,
+}));
+
+const handleTableChange: TableProps['onChange'] = (
+  pag: { pageSize: number; current: number },
+  filters: any,
+  sorter: any,
+) => {
+  run({
+    results: pag.pageSize,
+    page: pag?.current,
+    sortField: sorter.field,
+    sortOrder: sorter.order,
+    ...filters,
+  });
+};
+</script>
+
--- views/HomeView.vue
+++ views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import BasicTable from '@/components/BasicTable/BasicTable.vue';
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <BasicTable></BasicTable>
   </main>
 </template>
